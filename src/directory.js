d429236d165742aa05cbdb6a59694f2395ab43dd
unknown <bm_bentaiba@esi.dz>
2019-05-02 15:47:02 +0100

    refactor

--- src/directory.js
+++ src/directory.js
@@ -97,3 +97,37 @@ function filter(){
   let gender = document.getElementsByName("gender");
   console.log('gender', gender[0].checked);
 }
+
+
+/** login/signup */
+const loginBtn = document.getElementById('login');
+const signupBtn = document.getElementById('signup');
+
+loginBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode.parentNode;
+	Array.from(e.target.parentNode.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			signupBtn.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+signupBtn.addEventListener('click', (e) => {
+	let parent = e.target.parentNode;
+	Array.from(e.target.parentNode.classList).find((element) => {
+		if(element !== "slide-up") {
+			parent.classList.add('slide-up')
+		}else{
+			loginBtn.parentNode.parentNode.classList.add('slide-up')
+			parent.classList.remove('slide-up')
+		}
+	});
+});
+
+const connect = document.getElementById("connect")
+connect.addEventListener('click',(e)=>{let login=document.getElementById("form-structor") 
+login.style.display=="block" ? login.style.display="none":login.style.display="block"
+})
